903fbb117703d097f9517677b426e0bd8db32046
jakelord1 <jakelord1.vo@gmail.com>
2023-10-13 14:19:20 +0300

    Добавьте файлы проекта.

---
+++ OOL_w6d5/OOL_w6d5.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+
+
+
+typedef pair<string, string> Mpair;
+int main()
+{
+    setlocale(LC_ALL, " ");
+    map<string, string> dictionary;
+    string enter, enter2;
+    int choose = 0;
+    cout << "1 - Add\t2 - Delete\t3 - Find\t4 - Edit\tAny other - Exit\n";
+    do {
+        switch (choose) {
+        case 1: {
+            cin >> enter;
+            cin >> enter2;
+            dictionary.insert(Mpair(enter, enter2));
+            break;
+        }
+        case 2: {
+            cin >> enter;
+            auto find = dictionary.find(enter);
+            if (find != dictionary.end()) {
+                dictionary.erase(find);
+            }
+            break;
+        }
+        case 3: {
+            cin >> enter;
+            auto find = dictionary.find(enter);
+            if (find != dictionary.end()) {
+                cout << find->first << " - " << find->second << endl;
+            }
+            break;
+        }
+        case 4: {
+            cin >> enter;
+            auto find = dictionary.find(enter);
+            if (find != dictionary.end()) {
+                cin >> enter2;
+                dictionary[enter2] = enter2;
+            }
+            break;
+        }
+        default: {
+            exit(0);
+              }
+        }
+    } while (true);
+}
